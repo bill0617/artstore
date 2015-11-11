5ace1ee239869cc8a32b53f3cc48cc2551abbf98
Bill Hung <71base@livemail.tw>
2015-11-11 17:14:36 +0800

    upload picture

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,47 @@
+class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+  def index
+   @products = Product.all
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @photo = @product.build_photo
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+   @product = Product.find(params[:id])
+   @photo = @product.photo
+
+ end
+
+ def update
+   @product = Product.find(params[:id])
+
+   if @product.update(product_params)
+     redirect_to admin_products_path
+   else
+     render :edit
+   end
+ end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price,
+                                    photo_attributes: [:image, :id])
+
+  end
+end
