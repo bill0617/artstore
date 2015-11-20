37dfb533d50be42c120df00ada22bc4d7245765e
Bill Hung <71base@livemail.tw>
2015-11-20 14:01:47 +0800

    gem devise and create admin account by seed

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,12 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @products = Product.all
+  end
+
+
   def new
     @product = Product.new
   end
@@ -33,6 +41,8 @@ private
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
   end
+
+
 
 end
 
